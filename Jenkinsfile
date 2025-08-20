1f52693f361c8fff1055bdc957b4d414871976b5
Hemant <hpatel571989@gmail.com>
2025-08-20 21:19:58 +0530

    jenkins script initial setup

---
+++ Jenkinsfile
@@ -0,0 +1,3 @@
+buildPipeline {
+  "enableAutoDocumentation": true
+}
--- src/main/java/com/simple/webapp/config/DbConfig.java
+++ src/main/java/com/simple/webapp/config/DbConfig.java
@@ -1,4 +1,4 @@
-package com.simple.webapp;
+package com.simple.webapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
